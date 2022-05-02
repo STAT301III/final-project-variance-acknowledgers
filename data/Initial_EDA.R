eae0729f76dc2e8bd1665166e3defa30236dad26
unknown <willcichowski2023@u.northwestern.edu>
2022-05-01 20:48:41 -0500

    -new clean data

--- data/Initial_EDA.R
+++ data/Initial_EDA.R
@@ -7,13 +7,14 @@ library(skimr)
 library(janitor)
 library(corrplot)
 library(stringr)
+library(lubridate)
 
 #read in raw data
 uni <- read.csv('data/unprocessed/Unicorn_Companies.csv')
 
 
 #Do a quick skim of the data
-skim_without_charts(uni)
+skim_without_charts(uni_clean)
 
 
 #Here, we can see that there is no missingness in the data at all, but upon inspecting the data
@@ -22,25 +23,47 @@ skim_without_charts(uni)
 
 #Let's first turn the numerical columns that are currently characters to numerical:
 uni_clean <- uni %>%
-  mutate(Valuation = case_when(Valuation == "None" ~ NA,
-    TRUE ~ as.double(substring(Valuation...B., 2))), 
-         Date.Joined = case_when(Date.Joined == "None" ~ NA,
-           TRUE ~ as.Date(Date.Joined, "%m/%d/%Y")), 
-         Investors = case_when(Select.Inverstors == "None" ~ NA,
-           TRUE ~ Select.Inverstors),
-         Total.Raised = case_when(Total.Raised == "None" ~ NA,
-           substr(Total.Raised, nchar(Total.Raised), nchar(Total.Raised)) == "M" ~ as.double(substr(Total.Raised, 2, nchar(Total.Raised)-1))*1000000,
-           substr(Total.Raised, nchar(Total.Raised), nchar(Total.Raised)) == "B" ~ as.double(substr(Total.Raised, 2, nchar(Total.Raised)-1))*1000000000
-           ),
-         Investors.Count = case_when(Investors.Count == "None" ~ NA,
-           TRUE ~ as.integer(Investors.Count)),
-         Deal.Terms = case_when(Deal.Terms == "None" ~ NA,
-                                TRUE ~ as.integer(Deal.Terms)),
-         Portfolio.Exits = case_when(Portfolio.Exits == "None" ~ NA,
-                                TRUE ~ as.integer(Portfolio.Exits))
-  )
+  mutate(valuation = as.numeric(extract_numeric(Valuation...B.)) * 1000000000,
+         date_joined= extract_numeric(str_sub(Date.Joined, start= -4)),
+         investor_count = extract_numeric(Investors.Count),
+         deal_terms = extract_numeric(Deal.Terms),
+         portfolio_exits = extract_numeric(Portfolio.Exits),
+         total_raised = extract_numeric(Total.Raised) * 1000000000,
+         ) %>% 
+  select(-c(Date.Joined, Investors.Count, Valuation...B., Deal.Terms, Portfolio.Exits, Total.Raised)) %>%
+  clean_names()
+
+# taking investors out, then pivoting wider//onehotting
+uni_clean <-
+  uni_clean %>% 
+  mutate(select_inverstors = str_replace(select_inverstors, ",", "|"),
+         select_inverstors = str_replace(select_inverstors, ",", "|"),
+         select_inverstors = str_replace(select_inverstors, ",", "|"),
+         select_inverstors = str_replace(select_inverstors, ",", "|"),
+         select_inverstors = str_remove_all(select_inverstors, ' '),
+         select_inverstors = str_to_lower(select_inverstors)) %>% 
+  separate_rows(select_inverstors, sep = "\\|")
+
+# checking counts of investors
+one_hot_investors <- uni_clean %>% 
+  group_by(select_inverstors) %>% 
+  summarise(counts = n()) %>% 
+  arrange(desc(counts)) %>% 
+  filter(counts > 5)
+
+uni_clean <- 
+  uni_clean %>% filter(select_inverstors %in% one_hot_investors$select_inverstors)
+
+# now pivot wider
+uni_clean <- uni_clean[-c(1063, 367), ]
+uni_clean <- uni_clean %>% 
+  mutate(value = 1) %>% 
+  spread(select_inverstors, value, fill = 0)
+
 
 #With total raised, usually follows $100.4M type of structure but sometimes is None
+
+# function to fix data
 
 
 skim_without_charts(uni_clean)
