bdf3a5e4a8aca5d3d7ba9003c9dd7b3c6f28f643
mlorenz13 <matthewlorenz2023@u.northwestern.edu>
2022-06-08 11:36:18 -0500

    tuned linear regression for experiments 1 and 3

---
+++ linreg_fitting.R
@@ -0,0 +1,64 @@
+# load packages
+library(readr)
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(skimr)
+library(janitor)
+library(corrplot)
+library(stringr)
+library(lubridate)
+library(tidymodels)
+tidymodels_prefer()
+
+# load data and recipe
+load(file = "data/processed/exp_1_train.rda")
+load(file = "data/processed/exp_1_test.rda")
+load(file = "data/processed/exp_1_folds.rda")
+
+#Final iterations of recipes, 2 experiments, each with 
+#An iteration containing NLP and one Not
+
+#Only two for this because the second experiment is classification
+#and will be completed in the logistic regression section
+load("recipes/recipe_nlp_exp_1.rds")
+load("recipes/recipe_nlp_exp_3.rds")
+load("recipes/recipe_no_nlp_exp_1.rds")
+load("recipes/recipe_no_nlp_exp_3.rds")
+
+prep <- prep(recipe_no_nlp_1, exp_1_train)
+bake <- bake(prep, exp_1_train)
+
+sum(is.na(bake))
+
+
+# linear regression model initialisation
+lin_reg_model <- linear_reg(mode = "regression", 
+                            penalty = tune(),
+                            mixture = tune()) %>%
+  set_engine("lm")
+
+# set up workflow
+lin_reg_wf <-
+  workflow() %>% 
+  add_model(lin_reg_model) %>%
+  add_recipe(recipe_no_nlp_1)
+
+# create grid
+lin_reg_params <- hardhat::extract_parameter_set_dials(lin_reg_model)
+
+lin_reg_grid <- grid_regular(rf_params, levels = 3)
+
+control <- control_resamples(save_pred = TRUE)
+
+# tuning lin reg
+
+
+lin_reg_tuned_no_nlp_1 <- lin_reg_wf %>% 
+  tune_grid(exp_1_folds, grid = lin_reg_grid)
+
+#Save each object
+save(lin_reg_tuned_no_nlp_1, file = 'fit models/lin_reg_tuned_no_nlp_1.rda')
+
+
+
